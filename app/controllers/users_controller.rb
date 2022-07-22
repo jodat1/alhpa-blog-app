91bfc5bcf0f1de33fa0f4acecbf374ced909d72f
jodat1 <jodat.jabal1@gmail.com>
2022-07-22 16:46:29 +0500

    Added restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController <ApplicationController
+    before_action :require_same_user, only:[:edit, :update]
+    
     def index
         @users = User.all
     end
@@ -42,6 +44,13 @@ class UsersController <ApplicationController
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+        if current_user != @user
+            flash[:danger] = "You can only edit your own account"
+            redirect_to root_path
+        end
     end
 end
 
