c6ade567a79b26326ce158ce6c9122eabd137f70
jodat1 <jodat.jabal1@gmail.com>
2022-07-25 20:01:02 +0500

    edit categories and finalzed app

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,9 +23,25 @@ class CategoriesController < ApplicationController
         end
     end
 
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:success] = "You have successfully updated the category"
+            redirect_to category_path(@category)
+        else
+            render 'edit'
+        end
+
+    end
+
 
     def show
         @category = Category.find(params[:id])
+        @category_articles = @category.articles
     
     end
 
